7a1947f81c1dcf9837605cd1d0d0d927d61a3148
Zach Garman <zachgarman@gmail.com>
2016-10-11 14:04:22 -0500

    actually read the instructions this time

--- solution.sql
+++ solution.sql
@@ -20,13 +20,13 @@ FROM syntax_practice
 WHERE transactions_attempted >= 9;
 
 -- 5. Get the username and account balance of the 3 users with the highest balances, sort highest to lowest balance. NOTE: Research LIMIT
-SELECT *
+SELECT username, account_balance
 FROM syntax_practice
 ORDER BY account_balance DESC
 LIMIT 3;
 
 -- 6. Get the username and account balance of the 3 users with the lowest balances, sort lowest to highest balance.
-SELECT *
+SELECT username, account_balance
 FROM syntax_practice
 ORDER BY account_balance ASC
 LIMIT 3;
